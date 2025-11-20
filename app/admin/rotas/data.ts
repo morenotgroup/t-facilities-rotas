c4b69b276ebb7655b23c4a5ce7cc56449e239682
morenotgroup <gc@agenciataj.com>
2025-11-19 21:53:32 -0300

    Update data.ts

--- app/admin/rotas/data.ts
+++ app/admin/rotas/data.ts
@@ -1,31 +1,83 @@
 // app/admin/rotas/data.ts
+
+export type DiaSemana = 1 | 2 | 3 | 4 | 5 // seg a sex
+
 export type RotaModelo = {
   id: string
-  diaSemana: number // 1 a 5 (seg a sex)
+  diaSemana: DiaSemana
   colaboradorNome: string
   colaboradorEmail: string
   turno: 'Manhã' | 'Tarde'
-  ambientes: { idAmbiente: string; nomeAmbiente: string; ordem: number }[]
+  ambientes: {
+    idAmbiente: string
+    nomeAmbiente: string
+    ordem: number
+    prioridade: 'Alta' | 'Média' | 'Baixa'
+  }[]
 }
 
 export const rotasModeloFake: RotaModelo[] = [
   {
-    id: 'rota-seg-giulia',
+    id: 'seg-giulia-manha',
     diaSemana: 1,
-    colaboradorNome: 'Giulia',
+    colaboradorNome: 'Giulia Costa',
     colaboradorEmail: 'giulia@tgroup.com.br',
     turno: 'Manhã',
     ambientes: [
-      { idAmbiente: 'amb-1', nomeAmbiente: 'Recepção', ordem: 1 },
-      { idAmbiente: 'amb-2', nomeAmbiente: 'Sala GC', ordem: 2 },
+      {
+        idAmbiente: 'amb-1',
+        nomeAmbiente: 'Recepção',
+        ordem: 1,
+        prioridade: 'Alta',
+      },
+      {
+        idAmbiente: 'amb-2',
+        nomeAmbiente: 'Sala GC',
+        ordem: 2,
+        prioridade: 'Média',
+      },
     ],
   },
   {
-    id: 'rota-seg-mateus',
+    id: 'seg-mateus-manha',
     diaSemana: 1,
     colaboradorNome: 'Mateus',
     colaboradorEmail: 'mateus@tgroup.com.br',
     turno: 'Manhã',
-    ambientes: [{ idAmbiente: 'amb-3', nomeAmbiente: 'Backyard 209', ordem: 1 }],
+    ambientes: [
+      {
+        idAmbiente: 'amb-3',
+        nomeAmbiente: 'Backyard 209',
+        ordem: 1,
+        prioridade: 'Alta',
+      },
+      {
+        idAmbiente: 'amb-4',
+        nomeAmbiente: 'Banheiro Social Térreo',
+        ordem: 2,
+        prioridade: 'Alta',
+      },
+    ],
+  },
+  {
+    id: 'ter-giulia-manha',
+    diaSemana: 2,
+    colaboradorNome: 'Giulia Costa',
+    colaboradorEmail: 'giulia@tgroup.com.br',
+    turno: 'Manhã',
+    ambientes: [
+      {
+        idAmbiente: 'amb-2',
+        nomeAmbiente: 'Sala GC',
+        ordem: 1,
+        prioridade: 'Alta',
+      },
+      {
+        idAmbiente: 'amb-4',
+        nomeAmbiente: 'Banheiro Social Térreo',
+        ordem: 2,
+        prioridade: 'Média',
+      },
+    ],
   },
 ]
